23d9ea65ced078af390ccede104539c08011dbad
MemphisHydra <ilyasssonic19@gmail.com>
2026-05-15 16:36:04 +0100

    Bug Fixed : User cant check his Top Tracks

--- app/database/db.py
+++ app/database/db.py
@@ -2,12 +2,16 @@ import sqlite3
 
 DATABASE_PATH = "data/spotify_tracker.db"
 
+
 def create_connection():
     """
     Create a connection to the SQLite database.
     """
 
-    connection = sqlite3.connect(DATABASE_PATH, timeout=10)
+    connection = sqlite3.connect(
+        DATABASE_PATH,
+        timeout=10
+    )
 
     return connection
 
@@ -23,23 +27,27 @@ def create_tables():
 
     cursor.execute("""
     CREATE TABLE IF NOT EXISTS recent_tracks (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    spotify_track_id TEXT,
-    song_name TEXT,
-    artist_name TEXT,
-    played_at TEXT UNIQUE
-)
-""")
+
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+
+        track_id TEXT,
+        song_name TEXT,
+        artist_name TEXT,
+
+        played_at TEXT UNIQUE
+
+    )
+    """)
 
     connection.commit()
 
     connection.close()
 
 
-
-def insert_track(spotify_track_id, song_name, artist_name, played_at):
+def insert_track(track_id, song_name, artist_name, played_at):
     """
     Insert a track into the database.
+    Prevent duplicate listening events.
     """
 
     connection = create_connection()
@@ -49,14 +57,16 @@ def insert_track(spotify_track_id, song_name, artist_name, played_at):
 
         cursor.execute("""
         INSERT OR IGNORE INTO recent_tracks (
-            spotify_track_id,
+
+            track_id,
             song_name,
             artist_name,
             played_at
+
         )
         VALUES (?, ?, ?, ?)
         """, (
-            spotify_track_id,
+            track_id,
             song_name,
             artist_name,
             played_at
